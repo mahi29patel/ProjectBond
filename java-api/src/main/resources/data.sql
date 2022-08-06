049bb2cd37d6260aff03ab7bb7e718cb911580a1
Mahipatel29 <mahoo29patel@gmail.com>
2022-08-06 11:52:55 +0530

    add code

---
+++ java-api/src/main/resources/data.sql
@@ -0,0 +1,25 @@
+INSERT INTO dogs (id, name, age) VALUES (1, 'Einstein', 3);
+INSERT INTO dogs (id, name, age) VALUES (2, 'Kaya', 5);
+
+
+INSERT INTO book VALUES (1, 'book1');
+INSERT INTO book VALUES (2, 'book2');
+INSERT INTO book VALUES (3, 'book3');
+
+INSERT INTO users VALUES (101,'Riya', 'riya12@gmail.com', '1234', 'FICOps');
+INSERT INTO users VALUES (102, 'Mahi', 'mahi29@gmail.com', 'abc', 'FICTest');
+INSERT INTO users VALUES (103, 'Khushi', 'khushi32@gmail.com', 'ab12', 'FICOps');
+
+INSERT INTO BookUser VALUES (1, 102);
+INSERT INTO BookUser VALUES (2, 101);
+INSERT INTO BookUser VALUES (2, 103);
+INSERT INTO BookUser VALUES (3, 101);
+
+INSERT INTO security VALUES (1001, 201, 301, 'Honda', '2022-08-08',10,'Savings',1000,'Active');
+INSERT INTO security VALUES (1002, 202, 302, 'Honda', '2022-08-08',10,'Savings',1000,'Active');
+INSERT INTO security VALUES (1003, 203, 303, 'Honda', '2022-08-08',10,'Savings',1000,'Active');
+
+INSERT INTO trade VALUES (1,2,1001,2,'Active',560.7,1,'2022-08-08','2022-08-08');
+INSERT INTO trade VALUES (2,3,1003,2,'Active',560.7,1,'2022-08-08','2022-08-08');
+INSERT INTO trade VALUES (3,1,1002,2,'Active',560.7,1,'2022-08-08','2022-08-08');
+
---
+++ java-api/src/main/resources/schema.sql
@@ -0,0 +1,67 @@
+DROP TABLE IF EXISTS dogs;
+
+CREATE TABLE dogs (
+id INT NOT NULL,
+name VARCHAR(250) NOT NULL,
+age INT NOT NULL
+);
+
+DROP TABLE IF EXISTS book;
+
+CREATE TABLE book (
+                      Id INT PRIMARY KEY ,
+                      BookName VARCHAR(250) NOT NULL
+);
+
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+                      Id INT PRIMARY KEY ,
+                      Name VARCHAR(250) NOT NULL,
+                      Email Varchar(250) NOT NULL,
+                      Password VARCHAR(50) NOT NULL,
+                      Role Varchar(50) NOT NULL
+);
+
+DROP TABLE IF EXISTS security;
+
+CREATE TABLE security (
+                       Id INT PRIMARY KEY ,
+                       ISIN INT NOT NULL ,
+                       CUSIP INT NOT NULL,
+                       Issuer Varchar(250) NOT NULL,
+                       MaturityDate DATE NOT NULL ,
+                       Coupon INT NOT NULL,
+                       Type Varchar(30) NOT NULL,
+                       FaceValue DECIMAL(7,2)  NOT NULL,
+                       Status Varchar(20) NOT NULL
+);
+
+DROP TABLE IF EXISTS trade;
+
+CREATE TABLE trade (
+                       Id INT PRIMARY KEY ,
+                       BookId INT NOT NULL,
+                       SecurityId INT NOT NULL,
+                       Quantity INT NOT NULL,
+                       Status Varchar(20) NOT NULL,
+                       Price DECIMAL(7,2)  NOT NULL,
+                       Buy_Sell SMALLINT NOT NULL,
+                       TradeDate DATE NOT NULL ,
+                       SettlementDate DATE NOT NULL,
+
+                    foreign key (BookId) references book(Id),
+                    foreign key (SecurityId) references security(Id)
+);
+
+DROP TABLE IF EXISTS BookUser;
+
+CREATE TABLE BookUser (
+                       BookId  INT,
+                       UserId INT,
+                       PRIMARY KEY(BookId, UserId),
+                       foreign key (BookId) references book(Id),
+                       foreign key (UserId) references users(Id)
+);
+
+
