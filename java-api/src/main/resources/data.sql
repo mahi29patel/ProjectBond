3c2f5465360cd97742dc27f4499cb8cce8eb6602
Mahipatel29 <mahoo29patel@gmail.com>
2022-08-08 18:41:18 +0530

    column added

--- java-api/src/main/resources/data.sql
+++ java-api/src/main/resources/data.sql
@@ -15,9 +15,9 @@ INSERT INTO BookUser VALUES (2, 101);
 INSERT INTO BookUser VALUES (2, 103);
 INSERT INTO BookUser VALUES (3, 101);
 
-INSERT INTO security VALUES (1001, 201, 301, 'Honda', '2022-08-08',10,'Savings',1000,'Active');
-INSERT INTO security VALUES (1002, 202, 302, 'Honda', '2022-08-08',10,'Savings',1000,'Active');
-INSERT INTO security VALUES (1003, 203, 303, 'Honda', '2022-08-08',10,'Savings',1000,'Active');
+INSERT INTO security VALUES (1001, 201, 301, 'Honda', '2022-08-08',10,'Savings',1000,'Active',102);
+INSERT INTO security VALUES (1002, 202, 302, 'Honda', '2022-08-08',10,'Savings',1000,'Active',103);
+INSERT INTO security VALUES (1003, 203, 303, 'Honda', '2022-08-08',10,'Savings',1000,'Matured');
 
 INSERT INTO trade VALUES (1,1,1001,2,'Active',560.7,1,'2022-08-08','2022-08-08');
 INSERT INTO trade VALUES (2,3,1003,2,'Active',560.7,1,'2022-08-08','2022-08-08');
