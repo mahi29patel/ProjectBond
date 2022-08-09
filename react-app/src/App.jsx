0bf86ad99313c9d7308e551b74ece32483a4ed84
khushansh <khushanshjindal211@gmail.com>
2022-08-09 11:41:43 +0530

    Added CRUD Table

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -2,6 +2,7 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import './App.css';
+import CRUDTable from "./components/CRUDTable/MyCRUDTable";
 import Login from "./components/Login/Login";
 import Pets from "./components/pets/Pets";
 
@@ -10,7 +11,8 @@ const App = () => {
     <BrowserRouter>
       <Routes> 
           <Route exact path="/" element={<Login />} />  
-          <Route path="/pets" element = {<Pets />}> </Route> 
+          <Route path="/pets" element = {<Pets />}> </Route>
+          <Route path="/crud" element = {<CRUDTable />}> </Route> 
       </Routes>
     </BrowserRouter>
   );
