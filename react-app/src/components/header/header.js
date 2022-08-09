3e58c3c888657bdde17f34f69b9f1752bce05803
khushi2912 <foodaholicclasher@gmail.com>
2022-08-09 12:38:43 +0530

    Fix navbar links

--- react-app/src/components/header/header.js
+++ react-app/src/components/header/header.js
@@ -6,8 +6,21 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import {useNavigate} from 'react-router-dom';
 
 export const ButtonAppBar = () => {
+  const name = JSON.parse(localStorage.getItem("user")).name;
+  let navigate = useNavigate();
+
+  const goToProfile = () => {
+    navigate("/profile");
+  }
+
+  const logOut = () => {
+    localStorage.removeItem("user");
+    navigate("/");
+
+  }
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
@@ -16,8 +29,8 @@ export const ButtonAppBar = () => {
           <Typography variant="h5" component="div" sx={{ flexGrow: 4 }}>
            TEAM 11
           </Typography>
-          <Button style={{color:'blue', paddingRight:'30px', fontWeight: 'bold', fontSize:'20px'}}>My Profile</Button>
-          <Button style={{color:'blue', paddingRight:'30px', fontWeight: 'bold', fontSize:'20px'}}>Logout</Button>
+          <Button style={{color:'blue', paddingRight:'30px', fontWeight: 'bold', fontSize:'20px'}} onClick={goToProfile}> Hi {name}</Button>
+          <Button style={{color:'blue', paddingRight:'30px', fontWeight: 'bold', fontSize:'20px'}} onClick={logOut}>Logout</Button>
           
         </Toolbar>
       </AppBar>
